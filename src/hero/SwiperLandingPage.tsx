2573f3771f6bbc01355ebaa95feb668112371802
Ahmed abdeldaym <ahmedabdeldaem01000@gmail.com>
2025-08-12 09:38:24 +0300

    fix: update slider images

--- src/hero/SwiperLandingPage.tsx
+++ src/hero/SwiperLandingPage.tsx
@@ -3,12 +3,12 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css"; // تأكد تضيف استايل swiper
  
 const data = [
-  { id: 1, image: "/public/images/New folder/gallery1/image1.jpg" },
-  { id: 1, image: "/public/images/New folder/gallery1/image2.jpg" },
-  { id: 1, image: "/public/images/New folder/gallery1/image3.jpg" },
-  { id: 1, image: "/public/images/New folder/gallery1/image4.jpg" },
-  { id: 1, image: "/public/images/New folder/gallery1/image5.jpg" },
-  { id: 1, image: "/public/images/New folder/gallery1/image6.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image1.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image2.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image3.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image4.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image5.jpg" },
+  { id: 1, image: "../../public/images/New folder/gallery1/image6.jpg" },
  
 ];
 
